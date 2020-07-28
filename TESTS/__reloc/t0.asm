97a23bc6e0bd5d4faf5e2b9f8eb93a4563955bbc
Rickey Bowers Jr <bitRAKE@gmail.com>
2020-07-28 08:11:32 -0600

    first try

---
+++ TESTS/__reloc/t0.asm
@@ -0,0 +1,48 @@
+; proof of concept
+format relocatable binary
+virtual at 0
+fixupz::
+end virtual
+
+	use16
+mov ax,MyLabel
+mov ax,[MyLabel]
+
+	use32
+mov eax,MyLabel
+mov eax,[MyLabel]
+
+	use64
+mov rax,MyLabel
+mov rax,[MyLabel]
+
+MyLabel:
+
+include 'display.inc' ; pretty
+
+i = 0
+iAddr = 0
+while 1
+	load vType byte from fixupz:i
+	load fFlags byte from fixupz:i+1
+	load iAddr dword from fixupz:i+2
+	i = i + 6
+
+	vType = vType and $FF
+	fFlags = fFlags and $FF
+	iAddr = iAddr and $FFFFFFFF
+
+	if iAddr = 0
+	break
+	end if
+
+	if iAddr < MyLabel
+	display \
+	9,"value_type: ",<vType,16>,\
+	9,"format_flags: ",<fFlags,16>,\
+	9,"address: ",<iAddr,16>,\
+	13,10
+	else
+		break
+	end if
+end while
